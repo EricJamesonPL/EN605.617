f68fffa5dc4b72c558fb6554f04e9c61d4d47d2b
Eric Jameson <eric.jameson@peratonlabs.com>
2026-01-23 12:49:24 -0500

    Initial formatting of Module 0

--- module0/helloworld.cpp
+++ module0/helloworld.cpp
@@ -1,9 +1,8 @@
-// 'Hello World!' program 
- 
+// 'Hello World!' program
+
 #include <iostream>
- 
-int main()
-{
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    return 0;
 }
